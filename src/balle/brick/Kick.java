c8d739547a05bd615be7884f68d18ec6bbefd535
Saulius Lukauskas <luksaulius@gmail.com>
2012-01-25 15:14:03 +0000

    moved brick to ball-e

---
+++ src/balle/brick/Kick.java
@@ -0,0 +1,21 @@
+package brick;
+
+import lejos.nxt.Motor;
+
+public class Kick {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Controller control = new Controller();
+		
+		control.kick();
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e){
+			// 
+		}
+	}
+
+}
