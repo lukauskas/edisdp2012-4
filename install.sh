1a608b8c1b288969e640a43f8ed9df0ef08a71d3
Saulius Lukauskas <luksaulius@gmail.com>
2012-01-25 13:18:12 +0000

    lejos install script

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
+SDP_HOME=`pwd`
+LIB="$SDP_HOME/lib"
+
 function install_lejos {
-    local lLOCATION=/tmp/lejos_NXJ_0_8_5beta.tar.gz
+    local lLOCATION="`mktemp /tmp/lejos_NXJ_0_8_5beta.XXXXXX.tar.gz`"
 
-    wget http://downloads.sourceforge.net/project/lejos/lejos-NXJ/0.8.5beta/lejos_NXJ_0_8_5beta.tar.gz?r=&ts=1327495476&use_mirror=kent -O "$lLOCATION"
+    echo "> Downloading lejos to $lLOCATION"
 
-    tar xvf $lLOCATION
-    
+    wget -O "$lLOCATION" "http://downloads.sourceforge.net/project/lejos/lejos-NXJ/0.8.5beta/lejos_NXJ_0_8_5beta.tar.gz?r=&ts=1327495476&use_mirror=kent" \
+    && echo "> extracting $lLOCATION" \
+    && cd $LIB \
+    && tar xvf $lLOCATION \
+    && rm $lLOCATION
 }
+function cleanup {
+    rm -rf $LIB 
+    mkdir $LIB
+}
+cleanup
+install_lejos
