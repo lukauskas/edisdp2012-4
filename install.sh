f6a7a1b66e1cb5c7eb7110a8bb662e9d71654fc9
Saulius Lukauskas <luksaulius@gmail.com>
2012-01-25 14:04:22 +0000

    bluetooth install add

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 SDP_HOME=`pwd`
 LIB="$SDP_HOME/lib"
-
+LD_LIBRARY_DIR="$LIB/lib"
+function add_to_bashrc {
+    grep -Fxq "$1" ~/.bashrc || echo "$1" >> ~/.bashrc
+}
 function install_lejos {
     local lLOCATION="`mktemp /tmp/lejos_NXJ_0_8_5beta.XXXXXX.tar.gz`"
 
@@ -10,11 +13,28 @@ function install_lejos {
     && echo "> extracting $lLOCATION" \
     && cd $LIB \
     && tar xvf $lLOCATION \
-    && rm $lLOCATION
+    && rm $lLOCATION \
+
+    STR="export PATH=\$PATH:$LIB/lejos_nxt/bin"
+    add_to_bashrc $STR
 }
 function cleanup {
     rm -rf $LIB 
-    mkdir $LIB
+    mkdir -p $LIB
+    mkdir -p $LD_LIBRARY_DIR
+    STR="export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$LIB/LD_LIBRARY_DIR"
+
+}
+function install_bluetooth {
+    local lTMP_BLUETOOTH_LIB="`mktemp /tmp/libbluetooth.XXXXX.tar.gz`"
+    # Currently this is my dropbox acc public folder -- do not want to burden the repo with binaries -- Saulius  
+    # TODO: Put this in the repo when publishing
+    # 2013 student, if we forget this, find a way to compile libluetooth yourself
+    # that is what is being downloaded
+    wget "http://dl.dropbox.com/u/46248986/libbluetooth.tar.gz" -O $lTMP_BLUETOOTH_LIB \
+    && cd $LIB && tar xvf $lTMP_BLUETOOTH_LIB && cd $SDP_HOME && rm $lTMP_BLUETOOTH_LIB
 }
 cleanup
 install_lejos
+install_bluetooth
+
