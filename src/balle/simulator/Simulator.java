49157f4a3858c8c36db191b469fb683493971945
James Vaughan <james.e.vaughan@btinternet.com>
2012-01-26 23:08:27 +0000

    Fixed the compiler

--- src/balle/simulator/Simulator.java
+++ src/balle/simulator/Simulator.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class Simulator implements Controller {
 	
+	public static void main(String[] args) {
+		System.out.println("HI");
+	}
+	
 	public void backward() {
 		
 	}
