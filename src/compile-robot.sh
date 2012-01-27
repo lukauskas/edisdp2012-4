b78bebfd02196d81d81c94e9a9a5f0d79c1392c8
Saulius Lukauskas <luksaulius@gmail.com>
2012-01-27 16:27:27 +0000

    renamed milestone1 commands

--- src/compile-robot.sh
+++ src/compile-robot.sh
@@ -5,7 +5,7 @@ rm balle/controller/BluetoothController.class
 nxjc balle/brick/Controller.java balle/brick/BrickController.java balle/brick/Roboto.java balle/brick/Kick.java balle/brick/milestone1/RollAndKick.java balle/brick/milestone1/RollThroughField.java \
 && nxjlink balle.brick.Roboto -o Roboto.nxj \
 && nxjlink balle.brick.Kick -o M1Kick.nxj \
-&& nxjlink balle.brick.milestone1.RollAndKick -o RollAndKick.nxj \
-&& nxjlink balle.brick.milestone1.RollThroughField -o RollThroughField.nxj \
+&& nxjlink balle.brick.milestone1.RollAndKick -o M1RollAndKick.nxj \
+&& nxjlink balle.brick.milestone1.RollThroughField -o M1RollThroughField.nxj \
 && nxjpcc balle/bluetooth/Communicator.java balle/controller/BluetoothController.java balle/bluetooth/TestBluetooth.java \
 
