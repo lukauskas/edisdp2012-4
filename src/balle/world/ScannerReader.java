fc1f7694fc9c12c8d4250a578a8acdd145e0b770
Saulius Lukauskas <luksaulius@gmail.com>
2012-01-28 20:03:24 +0000

    made scanner run on a separate thread

--- src/balle/world/ScannerReader.java
+++ src/balle/world/ScannerReader.java
@@ -2,17 +2,24 @@ package balle.world;
 
 import java.util.Scanner;
 
-public class ScannerReader implements DataReader {
+public class ScannerReader extends Thread implements DataReader {
 
-	private Scanner scanner;
-	
-	public ScannerReader() {
-		scanner = new Scanner(System.in);
-	}
-	
-	@Override
-	public String nextLine() {
-		return scanner.nextLine();
-	}
+    private Scanner scanner;
+    private String  newestLine = null;
 
+    public ScannerReader() {
+        scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            newestLine = scanner.nextLine();
+        }
+    }
+
+    @Override
+    public String nextLine() {
+        return newestLine;
+    }
 }
