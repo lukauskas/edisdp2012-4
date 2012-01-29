cbd4410d558af556fdf0448ad2f5797371769fe3
Saulius Lukauskas <luksaulius@gmail.com>
2012-01-29 23:03:42 +0000

    fixed robot measurements and speeds

--- src/balle/brick/BrickController.java
+++ src/balle/brick/BrickController.java
@@ -1,125 +1,143 @@
 package balle.brick;
+
 import lejos.nxt.Motor;
 import lejos.robotics.navigation.TachoPilot;
 
-/** 
+/**
  * The Control class. Handles the actual driving and movement of the bot, once
  * BotCommunication has processed the commands.
  * 
  * That is -- defines the behaviour of the bot when it receives the command.
  * 
  * Adapted from SDP2011 groups 10 code -- original author shearn89
+ * 
  * @author sauliusl
  */
-public class BrickController implements Controller{
-	TachoPilot pilot;
-	public int maxPilotSpeed=30; // 20 for friendlies
-	
-	public final Motor LEFT_WHEEL  = Motor.B;
-	public final Motor RIGHT_WHEEL = Motor.C;
-	public final Motor KICKER      = Motor.A;
-	
-	public final float WHEEL_DIAMETER = 8.0f; // TODO: Measure these properly
-	public final float TRACK_WIDTH =  10.3f;
-	
-	public final int MAXIMUM_MOTOR_SPEED = 720;
+public class BrickController implements Controller {
+    TachoPilot         pilot;
+    public int         maxPilotSpeed       = 600;    // 20 for friendlies
 
-	public BrickController () {
-		
-		pilot = new TachoPilot(WHEEL_DIAMETER, TRACK_WIDTH, LEFT_WHEEL, RIGHT_WHEEL);
-		pilot.setMoveSpeed(maxPilotSpeed);
-		pilot.setTurnSpeed(45); // 45 has been working fine.
-		pilot.regulateSpeed(true);
-		LEFT_WHEEL.smoothAcceleration(true);
-		RIGHT_WHEEL.smoothAcceleration(true);
-	}
-	
-		
-	/* (non-Javadoc)
-	 * @see balle.brick.Controller#floatWheels()
-	 */
-	@Override
-	public void floatWheels(){
-		LEFT_WHEEL.flt();
-		RIGHT_WHEEL.flt();
-	}
-	/* (non-Javadoc)
-	 * @see balle.brick.Controller#stop()
-	 */
-	@Override
-	public void stop(){
-		pilot.stop();
-	}
-	/* (non-Javadoc)
-	 * @see balle.brick.Controller#travel(int)
-	 */
-	@Override
-	public void travel(int dist){
-		pilot.travel(dist);
-	}
-	/* (non-Javadoc)
-	 * @see balle.brick.Controller#kick()
-	 */
-	@Override
-	public void kick(){
-		KICKER.setSpeed(900);
-		KICKER.resetTachoCount();
-		KICKER.rotateTo(60);
-		KICKER.rotateTo(0);
-	}
-	
-	public float getTravelDistance() {
-		return pilot.getTravelDistance();
-	}
-	
-	public void reset() {
-		pilot.reset();
-	}
-    
-	private void setMotorSpeed(Motor motor, int speed)
-	{
-	    boolean forward = true;
-	 	if (speed < 0)
-	 	{
-	 		forward = false;
-	 		speed = -1 * speed;
-	 	}
-	 	
-	 	motor.setSpeed(speed);
-	 	if (forward) motor.forward();
-	 	else motor.backward();
-	}
-	@Override
-	public void setWheelSpeeds(int leftWheelSpeed, int rightWheelSpeed) {
-		if (leftWheelSpeed > MAXIMUM_MOTOR_SPEED) leftWheelSpeed = MAXIMUM_MOTOR_SPEED;
-		if (rightWheelSpeed > MAXIMUM_MOTOR_SPEED) rightWheelSpeed = MAXIMUM_MOTOR_SPEED;
-		
-		setMotorSpeed(LEFT_WHEEL, leftWheelSpeed);
-		setMotorSpeed(RIGHT_WHEEL, rightWheelSpeed);
-	}
+    public final Motor LEFT_WHEEL          = Motor.B;
+    public final Motor RIGHT_WHEEL         = Motor.C;
+    public final Motor KICKER              = Motor.A;
 
-	@Override
-	public int getMaximumWheelSpeed() {
-		return MAXIMUM_MOTOR_SPEED;
-	}
+    public final float WHEEL_DIAMETER      = 81.6f;  // milimetres
+                                                      // milimetres
+    public final float TRACK_WIDTH         = 130f;
 
+    public final int   MAXIMUM_MOTOR_SPEED = 720;
 
-	@Override
-	public void backward(int speed) {
-		// TODO Auto-generated method stub
-		
-	}
+    public BrickController() {
 
-	@Override
-	public void forward(int speed) {
-		pilot.setMoveSpeed(speed);
-	    pilot.forward();
-		
-	}
-	
-	@Override
-	public void rotate(int deg, int speed) {
-		pilot.setTurnSpeed(speed);
-		pilot.forward();		
-	}
+        pilot = new TachoPilot(WHEEL_DIAMETER, TRACK_WIDTH, LEFT_WHEEL,
+                RIGHT_WHEEL);
+        pilot.setMoveSpeed(maxPilotSpeed);
+        pilot.setTurnSpeed(45); // 45 has been working fine.
+        pilot.regulateSpeed(true);
+        LEFT_WHEEL.regulateSpeed(true);
+        RIGHT_WHEEL.regulateSpeed(true);
+        LEFT_WHEEL.smoothAcceleration(true);
+        RIGHT_WHEEL.smoothAcceleration(true);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see balle.brick.Controller#floatWheels()
+     */
+    @Override
+    public void floatWheels() {
+        LEFT_WHEEL.flt();
+        RIGHT_WHEEL.flt();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see balle.brick.Controller#stop()
+     */
+    @Override
+    public void stop() {
+        pilot.stop();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see balle.brick.Controller#travel(int)
+     */
+    @Override
+    public void travel(int dist) {
+        pilot.travel(dist);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see balle.brick.Controller#kick()
+     */
+    @Override
+    public void kick() {
+        KICKER.setSpeed(900);
+        KICKER.resetTachoCount();
+        KICKER.rotateTo(60);
+        KICKER.rotateTo(0);
+    }
+
+    public float getTravelDistance() {
+        return pilot.getTravelDistance();
+    }
+
+    public void reset() {
+        pilot.reset();
+    }
+
+    private void setMotorSpeed(Motor motor, int speed) {
+        boolean forward = true;
+        if (speed < 0) {
+            forward = false;
+            speed = -1 * speed;
+        }
+
+        motor.setSpeed(speed);
+        if (forward)
+            motor.forward();
+        else
+            motor.backward();
+    }
+
+    @Override
+    public void setWheelSpeeds(int leftWheelSpeed, int rightWheelSpeed) {
+        if (leftWheelSpeed > MAXIMUM_MOTOR_SPEED)
+            leftWheelSpeed = MAXIMUM_MOTOR_SPEED;
+        if (rightWheelSpeed > MAXIMUM_MOTOR_SPEED)
+            rightWheelSpeed = MAXIMUM_MOTOR_SPEED;
+
+        setMotorSpeed(LEFT_WHEEL, leftWheelSpeed);
+        setMotorSpeed(RIGHT_WHEEL, rightWheelSpeed);
+    }
+
+    @Override
+    public int getMaximumWheelSpeed() {
+        return MAXIMUM_MOTOR_SPEED;
+    }
+
+    @Override
+    public void backward(int speed) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void forward(int speed) {
+        pilot.setMoveSpeed(speed);
+        pilot.forward();
+
+    }
+
+    @Override
+    public void rotate(int deg, int speed) {
+        pilot.setTurnSpeed(speed);
+        pilot.rotate(deg);
+    }
 }
--- src/balle/brick/milestone1/RollThroughField.java
+++ src/balle/brick/milestone1/RollThroughField.java
@@ -7,48 +7,42 @@ import balle.brick.BrickController;
 
 public class RollThroughField {
 
-	private static void drawMessage(String message) {
-		LCD.clear();
-		LCD.drawString(message, 0, 0);
-		LCD.refresh();
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		BrickController controller = new BrickController();
-		TouchSensor sensorLeft = new TouchSensor(SensorPort.S1);
-		TouchSensor sensorRight = new TouchSensor(SensorPort.S2);
-		
-		boolean movingForward = false;
-		while (true)
-		{
-			if (sensorLeft.isPressed() || sensorRight.isPressed())
-			{
-				drawMessage("Whoops, wall!");
-				controller.stop();
-				controller.setWheelSpeeds(-controller.getMaximumWheelSpeed(), -controller.getMaximumWheelSpeed());
-				try {
-					Thread.sleep(200);
-					break;
-				}
-				catch (Exception e)
-				{
-					drawMessage(";/");
-					break;
-				}
-			}
-			if (!movingForward)
-			{
-				controller.setWheelSpeeds(controller.getMaximumWheelSpeed(), controller.getMaximumWheelSpeed());
-				drawMessage("Roll");
-				movingForward = true;
-			}			
-			else
-			{
-				drawMessage("Keep rollin, rollin..");
-			}
-		}
-	}
+    private static void drawMessage(String message) {
+        LCD.clear();
+        LCD.drawString(message, 0, 0);
+        LCD.refresh();
+    }
 
+    public static void main(String[] args) {
+
+        BrickController controller = new BrickController();
+        TouchSensor sensorLeft = new TouchSensor(SensorPort.S1);
+        TouchSensor sensorRight = new TouchSensor(SensorPort.S2);
+
+        boolean movingForward = false;
+        int speed = 400;
+        while (true) {
+            if (sensorLeft.isPressed() || sensorRight.isPressed()) {
+                drawMessage("Whoops, wall!");
+                controller.stop();
+                controller.setWheelSpeeds(-controller.getMaximumWheelSpeed(),
+                        -controller.getMaximumWheelSpeed());
+                try {
+                    Thread.sleep(200);
+                } catch (Exception e) {
+                    drawMessage(";/");
+                    break;
+                }
+                controller.rotate(180, 200);
+                break;
+            }
+            if (!movingForward) {
+                controller.forward(speed);
+                drawMessage("Roll");
+                movingForward = true;
+            } else {
+                drawMessage("Keep rollin, rollin..");
+            }
+        }
+    }
 }
