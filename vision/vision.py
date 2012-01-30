0191fa5526b1bab70deffc8379c17c37536ae0b8
Ben Kay <b.kay@sms.ed.ac.uk>
2012-01-30 20:54:32 +0000

    Added some vision preprocessing

--- vision/vision.py
+++ vision/vision.py
@@ -4,6 +4,7 @@ import time
 import cv
 import socket
 from SimpleCV import Image, Camera
+from preprocess import Preprocessor
 from features import Features
 from display import Gui
 
@@ -12,28 +13,6 @@ HOST = 'localhost'
 PORT = 28546 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect( (HOST, PORT) )
-
-def undistort(image):
-    intrinsics = cv.CreateMat(3, 3, cv.CV_64FC1)
-    cv.Zero(intrinsics)
-    #camera data
-    intrinsics[0, 0] = 1100.850708957251072
-    intrinsics[1, 1] = 778.955239997982062
-    intrinsics[2, 2] = 1.0
-    intrinsics[0, 2] = 348.898495232253822
-    intrinsics[1, 2] = 320.213734835526282
-    dist_coeffs = cv.CreateMat(1, 4, cv.CV_64FC1)
-    cv.Zero(dist_coeffs)
-    dist_coeffs[0, 0] = -0.326795877008420
-    dist_coeffs[0, 1] = 0.139445565548056
-    dist_coeffs[0, 2] = 0.001245710462327
-    dist_coeffs[0, 3] = -0.001396618726445
-    
-    size = cv.GetSize(image)
-    image2 = cv.CreateImage(size, image.depth, image.nChannels)
-    cv.Undistort2(image, image2, intrinsics, dist_coeffs)
-    
-    return image2
 
 def output(ents):
     for name in ['yellow', 'blue', 'ball']:
@@ -44,23 +23,20 @@ def output(ents):
             angle = entity.angle()
 
         if name == 'ball':
-            s.send('{0} {1}'.format(x, y) + ' ')
+            s.send('{0} {1} '.format(x, y))
         else:
-            s.send('{0} {1} {2}'.format(x, y, angle) + ' ')
+            s.send('{0} {1} {2} '.format(x, y, angle))
 
     s.send(str(int(time.time() * 1000)) + " \n")
     
-
-
-#try:
 if len(sys.argv) > 1:
     pitchnum = int(sys.argv[1])
 else:
     # Default to the main pitch
     pitchnum = 0
 
-cap = Camera() #Capture()
-
+cap = Camera()
+preprocessor = Preprocessor()
 features = Features(pitchnum)
 gui = Gui.getGui()
 
@@ -69,6 +45,8 @@ while True:
     #frame = Image('global05.jpg')
     #frame = Image(frame)
 
+    frame = preprocessor.preprocess(frame)
+    
     gui.updateBase(frame)
 
     ents = features.extractFeatures(frame)
@@ -84,5 +62,3 @@ while True:
         break
 
 s.close()
-#finally:
-#    cap.stop()
