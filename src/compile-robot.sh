15c6362a8d56d6e7ba80b0876b8c5147ec2b308c
Mindaugas Dabulskis <mindausai@gmail.com>
2012-01-31 21:39:48 +0000

    implemented penalty kick into the brick

--- src/compile-robot.sh
+++ src/compile-robot.sh
@@ -2,9 +2,10 @@ rm balle/brick/*.class
 rm balle/bluetooth/*.class
 rm balle/controller/BluetoothController.class
 
-nxjc balle/brick/Controller.java balle/brick/BrickController.java balle/brick/Roboto.java balle/brick/Kick.java balle/brick/milestone1/RollAndKick.java balle/brick/milestone1/RollThroughField.java \
+nxjc balle/brick/Controller.java balle/brick/BrickController.java balle/brick/Roboto.java balle/brick/Kick.java balle/brick/milestone1/RollAndKick.java balle/brick/milestone1/RollThroughField.java balle/brick/PenaltyKick.java \
 && nxjlink balle.brick.Roboto -o Roboto.nxj \
 && nxjlink balle.brick.Kick -o M1Kick.nxj \
+&& nxjlink balle.brick.PenaltyKick -o M1PenaltyKick.nxj \
 && nxjlink balle.brick.milestone1.RollAndKick -o M1RollAndKick.nxj \
 && nxjlink balle.brick.milestone1.RollThroughField -o M1Roll.nxj \
 && nxjpcc balle/bluetooth/Communicator.java balle/controller/BluetoothController.java balle/bluetooth/TestBluetooth.java \
