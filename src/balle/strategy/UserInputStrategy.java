1b94297eddeb7830e4fac93c2bf566f0edb0f308
James Vaughan <james@James-Laptop.(none)>
2012-02-02 11:52:14 +0000

    Added to softbot, changed UserInputStrategy

--- src/balle/strategy/UserInputStrategy.java
+++ src/balle/strategy/UserInputStrategy.java
@@ -44,11 +44,11 @@ public class UserInputStrategy extends AbstractStrategy {
         	System.out.println(cmd);
                 
                 switch (cmd) {
-                case 'w':       controller.setWheelSpeeds( 1,  1); break;
-                case 'a':       controller.setWheelSpeeds(-1,  1); break;
-                case 's':       controller.setWheelSpeeds( 0,  0); break;
-                case 'd':       controller.setWheelSpeeds( 1, -1); break;
-                case 'x':       controller.setWheelSpeeds(-1, -1); break;
+                case 'w':       controller.setWheelSpeeds( 99,  99); break;
+                case 'a':       controller.setWheelSpeeds(-99,  99); break;
+                case 's':       controller.setWheelSpeeds(  0,   0); break;
+                case 'd':       controller.setWheelSpeeds( 99, -99); break;
+                case 'x':       controller.setWheelSpeeds(-99, -99); break;
                 }
                 
         }
