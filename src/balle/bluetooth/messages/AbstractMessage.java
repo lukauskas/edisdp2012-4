cfd56e767633ff5d622dc0595c4cf5dd30d6519a
Saulius Lukauskas <luksaulius@gmail.com>
2012-02-03 14:45:19 +0000

    opcode decoding implemented

--- src/balle/bluetooth/messages/AbstractMessage.java
+++ src/balle/bluetooth/messages/AbstractMessage.java
@@ -3,12 +3,13 @@ package balle.bluetooth.messages;
 public abstract class AbstractMessage {
 
     public final static int BITS_FOR_OPCODE = 3;
+    public final static int BITS_PER_INT    = 32;
 
     public abstract int getOpcode();
 
     public abstract int hash();
 
     public static final int extactOpcodeFromEncodedMessage(int message) {
-        return (message & 0xE000000) >> 19;
+        return message >>> (BITS_PER_INT - BITS_FOR_OPCODE);
     }
 }
