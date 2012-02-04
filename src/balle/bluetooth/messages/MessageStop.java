4d502947346db11d090168bcb80bacb1d3eb8cb1
Saulius Lukauskas <luksaulius@gmail.com>
2012-02-04 17:30:46 +0000

    Implemented MessageStop

---
+++ src/balle/bluetooth/messages/MessageStop.java
@@ -0,0 +1,35 @@
+package balle.bluetooth.messages;
+
+public class MessageStop extends AbstractSingleArgMessage {
+
+    /**
+     * Instantiates a new message stop.
+     * 
+     * @param floatWheels
+     *            whether to float wheels or not. 1: floatWheels, 0: stop in
+     *            place.
+     * @throws InvalidArgumentException
+     *             if argument provided is invalid
+     */
+    public MessageStop(int floatWheels) throws InvalidArgumentException {
+        super(floatWheels);
+        if ((floatWheels != 0) && (floatWheels != 1))
+            throw new InvalidArgumentException("floatWheels should either be 0 or 1");
+    }
+
+    @Override
+    public int getOpcode() {
+        return 1;
+    }
+
+    /**
+     * Returns whether the robot should stop in place or "float the wheels" and
+     * roll freely until stop.
+     * 
+     * @return true if the robot should float wheels
+     */
+    public boolean floatWheels() {
+        return getArgument() == 1;
+    }
+
+}
