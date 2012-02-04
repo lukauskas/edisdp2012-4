4336ba609de491cbf2b86f33d62b094f8dfbd273
Saulius Lukauskas <luksaulius@gmail.com>
2012-02-04 21:59:40 +0000

    fixed compile robot file

--- src/compile-robot.sh
+++ src/compile-robot.sh
@@ -1,8 +1,9 @@
 rm balle/brick/*.class 
 rm balle/bluetooth/*.class
+rm balle/bluetooth/messages/*.class
 rm balle/controller/BluetoothController.class
 
-nxjc balle/controller/Controller.java balle/brick/BrickController.java balle/brick/Roboto.java balle/brick/Kick.java balle/brick/milestone1/RollAndKick.java balle/brick/milestone1/RollThroughField.java balle/brick/PenaltyKick.java \
+nxjc balle/bluetooth/messages/*.java balle/controller/Controller.java balle/brick/BrickController.java balle/brick/Roboto.java balle/brick/Kick.java balle/brick/milestone1/RollAndKick.java balle/brick/milestone1/RollThroughField.java balle/brick/PenaltyKick.java \
 && nxjlink balle.brick.Roboto -o Roboto.nxj \
 && nxjlink balle.brick.Kick -o M1Kick.nxj \
 && nxjlink balle.brick.PenaltyKick -o M1PenaltyKick.nxj \
