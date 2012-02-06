80f652dc08b8275a9ef10cf9836bb18443abf024
Saulius Lukauskas <luksaulius@gmail.com>
2012-02-06 18:32:55 +0000

    Fixed the build that was caused by an unsolved conflict

--- src/balle/strategy/pFStrategy/PFPlanning.java
+++ src/balle/strategy/pFStrategy/PFPlanning.java
@@ -4,111 +4,100 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PFPlanning {
-	Pos robot;
-	PointObject opponent;
-	PointObject ball;
-	double default_power = 5;
-	RobotConf config;
-	double Stopdistance = 5;
-	List<Object> objects;
-	// power for opponent.
-	double opponentPower;
-	// influence distance for opponent
-	double opponentInf;
-	// power for goal location.
-	double ballPower;
-	// power orientation, extended potential field.
-	double opponentAlphaPower;
+    Pos          robot;
+    PointObject  opponent;
+    PointObject  ball;
+    double       default_power = 5;
+    RobotConf    config;
+    double       Stopdistance  = 5;
+    List<Object> objects;
+    // power for opponent.
+    double       opponentPower;
+    // influence distance for opponent
+    double       opponentInf;
+    // power for goal location.
+    double       ballPower;
+    // power orientation, extended potential field.
+    double       opponentAlphaPower;
 
-	// Constructor for PFPlanning:
-	// conf: Configuration parameters of the robot.
-	// opponentPower: Repulsive power for opponent
-	// opponentInf: Influence distance for opponent.
-	// ballPower: Power for goal position
-	public PFPlanning(RobotConf conf, double opponentPower, double opponentInf,
-			double targetPower) {
-		this.config = conf;
-		objects = new ArrayList<Object>();
-		this.opponentPower = opponentPower;
-		this.opponentInf = opponentInf;
-		this.ballPower = targetPower;
-	}
+    // Constructor for PFPlanning:
+    // conf: Configuration parameters of the robot.
+    // opponentPower: Repulsive power for opponent
+    // opponentInf: Influence distance for opponent.
+    // ballPower: Power for goal position
+    public PFPlanning(RobotConf conf, double opponentPower, double opponentInf,
+            double targetPower) {
+        this.config = conf;
+        objects = new ArrayList<Object>();
+        this.opponentPower = opponentPower;
+        this.opponentInf = opponentInf;
+        this.ballPower = targetPower;
+    }
 
-	// Constructor for PFPlanning:
-	// conf: Configuration parameters of the robot.
-	// opponentPower: Repulsive power for opponent
-	// opponentInf: Influence distance for opponent.
-	// ballPower: Power for goal position
-	// alpha: Orientation power
-	public PFPlanning(RobotConf conf, double opponentPower, double opponentInf,
-			double targetPower, double alpha) {
-		this.config = conf;
-		objects = new ArrayList<Object>();
-		this.opponentPower = opponentPower;
-		this.opponentInf = opponentInf;
-		this.ballPower = targetPower;
-		this.opponentAlphaPower = alpha;
-	}
+    // Constructor for PFPlanning:
+    // conf: Configuration parameters of the robot.
+    // opponentPower: Repulsive power for opponent
+    // opponentInf: Influence distance for opponent.
+    // ballPower: Power for goal position
+    // alpha: Orientation power
+    public PFPlanning(RobotConf conf, double opponentPower, double opponentInf,
+            double targetPower, double alpha) {
+        this.config = conf;
+        objects = new ArrayList<Object>();
+        this.opponentPower = opponentPower;
+        this.opponentInf = opponentInf;
+        this.ballPower = targetPower;
+        this.opponentAlphaPower = alpha;
+    }
 
+    private void init(Pos robot, Pos opponent, Point ball, boolean orig) {
+        PointObject opponentObj = new PointObject(opponent.getLocation(),
+                opponentPower, opponentInf, opponentAlphaPower);
+        PointObject ballObj = new PointObject(ball, ballPower, Double.MAX_VALUE);
+        this.robot = robot;
+        this.opponent = opponentObj;
+        this.ball = ballObj;
 
-	private void init(Pos robot, Pos opponent, Point ball,
-			boolean orig)
-	{
-		PointObject opponentObj = new PointObject(opponent.getLocation(),
-				opponentPower, opponentInf, opponentAlphaPower);
-		PointObject ballObj = new PointObject(ball, ballPower, Double.MAX_VALUE);
-		this.robot = robot;
-		this.opponent = opponentObj;
-		this.ball = ballObj;
-		
-	}
-	public VelocityVec update(Pos robot, Pos opponent,Point goal, Point ball,
-			boolean orig){
-		init(robot,opponent,ball,orig);
-		Vector vgoal=new Vector(goal);
-		Vector vball=new Vector(ball);
-		Vector vres=vgoal.subtract(vball);
-		Vector threshold=new Vector(20,20);
-		Vector finalres=vres.subtract(threshold);
-		PointObject obj=new PointObject(finalres, 10000, this.opponentInf);
-		List<Object> complList =  new ArrayList<Object>(objects);
-		complList.add((Object) this.opponent);
-		complList.add(obj);
-		Vector res = GoTo(complList, this.ball, robot.getLocation());
-<<<<<<< HEAD
-/*		if (Runner.DEBUG) {
-			//System.out.println("Result Vector: " + res.toString());
-		}*/
-=======
-//		if (Runner.DEBUG) {
-//			//System.out.println("Result Vector: " + res.toString());
-//		}
->>>>>>> 61e4de4b673159bdc8b0d093e4b2e3a9d1c9677e
-		if (orig)
-			return new VelocityVec(res.getX(),res.getY());
-		else
-			return getVelocity(res, robot);
+    }
 
+    public VelocityVec update(Pos robot, Pos opponent, Point goal, Point ball,
+            boolean orig) {
+        init(robot, opponent, ball, orig);
+        Vector vgoal = new Vector(goal);
+        Vector vball = new Vector(ball);
+        Vector vres = vgoal.subtract(vball);
+        Vector threshold = new Vector(20, 20);
+        Vector finalres = vres.subtract(threshold);
+        PointObject obj = new PointObject(finalres, 10000, this.opponentInf);
+        List<Object> complList = new ArrayList<Object>(objects);
+        complList.add(this.opponent);
+        complList.add(obj);
+        Vector res = GoTo(complList, this.ball, robot.getLocation());
 
-	}
+        if (orig)
+            return new VelocityVec(res.getX(), res.getY());
+        else
+            return getVelocity(res, robot);
 
-	// Given Pos of different objects, this function will create PointObjects to
-	// be used later.
-	// robot: robot Pos.
-	// opponent: opponent Pos.
-	// ball: ball Location.
-	// srr: add surrounding objects for target position to approach the target
-	// position
-	// from specific direction.
-	// orig: return original vector, if unset return vector is left,right wheels
-	// velocity, if
-	// if set it will return the basic velocity vector created by PFPlanner.
-	public VelocityVec update(Pos robot, Pos opponent, Point ball,
+    }
+
+    // Given Pos of different objects, this function will create PointObjects to
+    // be used later.
+    // robot: robot Pos.
+    // opponent: opponent Pos.
+    // ball: ball Location.
+    // srr: add surrounding objects for target position to approach the target
+    // position
+    // from specific direction.
+    // orig: return original vector, if unset return vector is left,right wheels
+    // velocity, if
+    // if set it will return the basic velocity vector created by PFPlanner.
+    public VelocityVec update(Pos robot, Pos opponent, Point ball,
 			boolean orig) {
 
 		init(robot,opponent,ball,orig);
 		List<Object> complList =  new ArrayList<Object>(objects);
-		complList.add((Object) this.opponent);
+		complList.add(this.opponent);
 		Vector res = GoTo(complList, this.ball, robot.getLocation());
 		Vector vball=new Vector(ball);
 		Vector vrobot=new Vector(robot.getLocation());
@@ -132,40 +121,40 @@ public class PFPlanning {
 
 	}
 
-	// Adds static object in the arena to list of static obstacles.
-	public void AddObjects(Object r) {
-		objects.add(r);
-	}
+    // Adds static object in the arena to list of static obstacles.
+    public void AddObjects(Object r) {
+        objects.add(r);
+    }
 
-	// Actual function which computes the the next velocity vector to be applied
-	// to the robor.
-	public Vector GoTo(List<Object> obstacles, PointObject dest_obj,
-			Point start_point) {
-		// calculate distance so if we reached the target position we stop.
-		double dist = Math.sqrt((start_point.getX() - dest_obj.getX())
-				* (start_point.getX() - dest_obj.getX())
-				+ (start_point.getY() - dest_obj.getY())
-				* (start_point.getY() - dest_obj.getY()));
-		if (dist < Stopdistance) {
-			return new Vector(0, 0);
-		}
+    // Actual function which computes the the next velocity vector to be applied
+    // to the robor.
+    public Vector GoTo(List<Object> obstacles, PointObject dest_obj,
+            Point start_point) {
+        // calculate distance so if we reached the target position we stop.
+        double dist = Math.sqrt((start_point.getX() - dest_obj.getX())
+                * (start_point.getX() - dest_obj.getX())
+                + (start_point.getY() - dest_obj.getY())
+                * (start_point.getY() - dest_obj.getY()));
+        if (dist < Stopdistance) {
+            return new Vector(0, 0);
+        }
 
-		Vector rep = new Vector(0, 0);
-		// iterate through all obstacles and compute sum of all repulsive
-		// vectors
-		for (int i = 0; i < obstacles.size(); i++) {
-			rep = rep.add(obstacles.get(i).getVector(start_point, true));
-		}
-		// Compute attractive vector.
-		Vector att = dest_obj.getVector(start_point, false);
+        Vector rep = new Vector(0, 0);
+        // iterate through all obstacles and compute sum of all repulsive
+        // vectors
+        for (int i = 0; i < obstacles.size(); i++) {
+            rep = rep.add(obstacles.get(i).getVector(start_point, true));
+        }
+        // Compute attractive vector.
+        Vector att = dest_obj.getVector(start_point, false);
 
-		return att.add(rep);
+        return att.add(rep);
 
-	}
+    }
 
-	// Extended Potential Field.This function takes into account the orientation
-	// of Robot.
-	public Vector GoTo(List<Object> obstacles, PointObject dest_obj,
+    // Extended Potential Field.This function takes into account the orientation
+    // of Robot.
+    public Vector GoTo(List<Object> obstacles, PointObject dest_obj,
 			Pos start_point) {
 		// calculate distance so if we reached the target position we stop.
 		double dist = Math.sqrt((start_point.getLocation().getX() - dest_obj
@@ -199,21 +188,21 @@ public class PFPlanning {
 
 	}
 
-	// This function translates linear and angular velocities to left and right
-	// velocities.
-	private VelocityVec CvtVelocity(double Vlin, double VAng, double r) {
-		double left = Vlin - r * Math.sin(VAng);
-		double right = Vlin + r * Math.sin(VAng);
-		VelocityVec vector = new VelocityVec(left, right);
-		// System.out.println("r: " + r+" left" + left + "right" + right);
-		return vector;
-	}
+    // This function translates linear and angular velocities to left and right
+    // velocities.
+    private VelocityVec CvtVelocity(double Vlin, double VAng, double r) {
+        double left = Vlin - r * Math.sin(VAng);
+        double right = Vlin + r * Math.sin(VAng);
+        VelocityVec vector = new VelocityVec(left, right);
+        // System.out.println("r: " + r+" left" + left + "right" + right);
+        return vector;
+    }
 
-	// Given current Pos of the robot and current velocity this function
-	// computes linear and angular
-	// velocities for a differential drive robot using input parameters of the
-	// robot.
-	private VelocityVec getVelocity(Vector inputVel, Pos current) {
+    // Given current Pos of the robot and current velocity this function
+    // computes linear and angular
+    // velocities for a differential drive robot using input parameters of the
+    // robot.
+    private VelocityVec getVelocity(Vector inputVel, Pos current) {
 
 		
 		double size = inputVel.size();
@@ -265,5 +254,4 @@ public class PFPlanning {
 >>>>>>> 61e4de4b673159bdc8b0d093e4b2e3a9d1c9677e
 		return CvtVelocity(Vlin, Vang, config.getr());
 	}
-
 }
