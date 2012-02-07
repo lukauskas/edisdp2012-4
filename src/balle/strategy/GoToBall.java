cb91e4294a346d2c2afde13dd815cad39b3e6722
Saulius Lukauskas <luksaulius@gmail.com>
2012-02-07 22:33:26 +0000

    GoToBall

--- src/balle/strategy/GoToBall.java
+++ src/balle/strategy/GoToBall.java
@@ -3,6 +3,7 @@ package balle.strategy;
 import balle.controller.Controller;
 import balle.world.AbstractWorld;
 import balle.world.Coord;
+import balle.world.FieldObject;
 import balle.world.Robot;
 
 public class GoToBall extends AbstractStrategy {
@@ -24,7 +25,11 @@ public class GoToBall extends AbstractStrategy {
     @Override
     protected void aiMove(Controller controller) {
         // Assume ball is static
-        Coord target = getSnapshot().getBall().getPosition();
+        if (getSnapshot() == null)
+            return;
+
+        FieldObject ball = getSnapshot().getBall();
+        Coord target = (ball != null) ? ball.getPosition() : null;
         Robot robot = getSnapshot().getBalle();
 
         Coord currentPosition = robot != null ? robot.getPosition() : null;
@@ -32,7 +37,7 @@ public class GoToBall extends AbstractStrategy {
             System.out.println("Cannot see ball or self");
             return;
         }
-
+        System.out.println("Ball " + target.getX() + " " + target.getY());
         if ((currentPosition.dist(target) - DISTANCE_THRESHOLD) < EPSILON) {
             if (isMoving) {
                 System.out.println("Target reached");
@@ -43,8 +48,13 @@ public class GoToBall extends AbstractStrategy {
         } else {
             double angleToTarget = target.sub(currentPosition).orientation()
                     .radians();
-            angleToTarget -= robot.getOrientation().radians();
-            System.out.println("Angle to target: " + angleToTarget);
+
+            angleToTarget = robot.getOrientation().radians() - angleToTarget;
+            angleToTarget *= -1;
+            // System.out.println("Angle to target2: " + angleToTarget);
+            // System.out
+            // .println("Dist to target:" + target.dist(currentPosition));
+            System.out.println(getSnapshot().getTimestamp());
         }
 
     }
