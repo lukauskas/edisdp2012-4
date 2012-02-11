bccfb8f057c35da60418d2773b98ddbed8e98f0b
Saulius Lukauskas <luksaulius@gmail.com>
2012-02-11 20:05:09 +0000

    Removed the debug line in communicator send so it does not spam stdin

--- src/balle/bluetooth/Communicator.java
+++ src/balle/bluetooth/Communicator.java
@@ -56,7 +56,7 @@ public class Communicator {
      */
     public boolean send(Integer message) {
         try {
-            System.err.println("Sending: " + Integer.toHexString(message));
+            // System.err.println("Sending: " + Integer.toHexString(message));
             dos.writeInt(message);
             dos.flush();
             return true;
