272d04a7c64e142a4ba1455c4209e51b31f7a0c9
Saulius Lukauskas <luksaulius@gmail.com>
2012-02-12 20:27:21 +0000

    fixed bugs in Strategy_M2 distance calculation with paulius

--- src/balle/strategy/Strategy_M2.java
+++ src/balle/strategy/Strategy_M2.java
@@ -8,119 +8,117 @@ import balle.world.Robot;
 
 public class Strategy_M2 extends AbstractStrategy {
 
-	private double EPSILON = 0.000001;
-	private double DISTANCE_THRESHOLD = 0.1;
-	private int INIT_SPEED = 220;
-	private int speed = INIT_SPEED;
-	private double timer_start;
-	private double timer_travel;
-	boolean reachedBall = false;
-	float distance = 0;
+    private static final double DISTANCE_TO_TRAVEL    = 1;
+    private double              EPSILON               = 0.000001;
+    private double              DISTANCE_THRESHOLD    = 0.13;
+    private int                 INIT_SPEED            = 400;
+    private int                 speed                 = INIT_SPEED;
+    boolean                     reachedBall           = false;
+    float                       distance              = 0;
+    private Coord               startCoord            = null;
+    private boolean             assumeDistanceReached = false;
 
-	public Strategy_M2(Controller controller, AbstractWorld world) {
-		super(controller, world);
-		// TODO Auto-generated constructor stub
-	}
+    public Strategy_M2(Controller controller, AbstractWorld world) {
+        super(controller, world);
+        // TODO Auto-generated constructor stub
+    }
 
-	@Override
-	protected void aiStep() {
-		// TODO Auto-generated method stub
+    @Override
+    protected void aiStep() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	protected void aiMove(Controller controller) {
-		// TODO Auto-generated method stub
-		if (getSnapshot() == null) {
-			System.out.println("No Snapshot");
-			return;
-		}
+    protected boolean reachedDistance() {
+        if (assumeDistanceReached)
+            return true;
+        if (startCoord == null)
+            return false;
 
-		FieldObject ball = getSnapshot().getBall();
-		Coord target = (ball != null) ? ball.getPosition() : null;
+        Coord currentCoord = getSnapshot().getBalle().getPosition();
+        if (currentCoord == null)
+            return true; // TODO: is this right?
 
-		Robot robot = getSnapshot().getBalle();
-		Coord currentPosition = robot != null ? robot.getPosition() : null;
+        double distanceTravelled = currentCoord.sub(startCoord).abs();
+        System.out.println("Distance travelled: " + distanceTravelled + " m");
+        boolean result = distanceTravelled >= DISTANCE_TO_TRAVEL;
+        assumeDistanceReached = result;
+        return result;
+    }
 
-		if ((target == null) || (currentPosition == null)) {
-			System.out.println("Vision Error, Cannot see Ball or Self");
-			return;
-		}
+    @Override
+    protected void aiMove(Controller controller) {
+        if (getSnapshot() == null) {
+            System.out.println("No Snapshot");
+            return;
+        }
 
-		// Ball in front of robot
-		if ((currentPosition.dist(target) - DISTANCE_THRESHOLD <= EPSILON) && (distance <= 30)) {
-			System.out.println("Reached the Ball");
-			if (reachedBall == false) {
-				timer_start = System.currentTimeMillis();
-			}
-			speed = INIT_SPEED;
-			controller.forward(speed);
-			reachedBall = true;
-			timer_travel = (System.currentTimeMillis() - timer_start) / 1000;
-			distance = distance + (float) (timer_travel * speed/360);
+        FieldObject ball = getSnapshot().getBall();
+        Coord target = (ball != null) ? ball.getPosition() : null;
 
-		}
+        Robot robot = getSnapshot().getBalle();
+        Coord currentPosition = robot != null ? robot.getPosition() : null;
 
-		// Ball away from robot
-		
-		if (((currentPosition.dist(target) - DISTANCE_THRESHOLD) > EPSILON) && (distance <= 30)) {
-			
-			double angleToTarget = target.sub(currentPosition).orientation();
-			
-			double currentOrientation = robot.getOrientation()
-			.atan2styleradians();
+        if ((target == null) || (currentPosition == null)) {
+            System.out.println("Vision Error, Cannot see Ball or Self");
+            return;
+        }
 
-			if ((speed <= 720) && (Math.abs(angleToTarget) <= 5)) {
-				speed = speed + 100;
-				controller.forward(speed);
-				System.out.println("Speeding Up");
-				if (reachedBall == true) {
-					timer_travel = (System.currentTimeMillis() - timer_start) / 1000;
-					distance = distance + (float) (timer_travel * speed/360);
-				}
-				
+        if (reachedDistance()) {
+            System.out.println("Reached distance, stopping!");
+            controller.stop();
+            return;
+        }
 
-			} else {
-				
+        // Ball in front of robot
+        if (currentPosition.dist(target) - DISTANCE_THRESHOLD <= EPSILON) {
+            System.out.println("Reached the Ball");
+            if (reachedBall == false) {
+                System.out.println("Setting start coordinate");
+                startCoord = getSnapshot().getBalle().getPosition();
+            }
+            speed = INIT_SPEED;
+            controller.forward(speed);
+            reachedBall = true;
+        } else {
+            // Ball away from robot
+            double angleToTarget = target.sub(currentPosition).orientation();
 
-				double turnLeftAngle, turnRightAngle;
+            double currentOrientation = robot.getOrientation()
+                    .atan2styleradians();
 
-				if (angleToTarget > currentOrientation) {
-					turnLeftAngle = angleToTarget - currentOrientation;
-					turnRightAngle = currentOrientation
-							+ (2 * Math.PI - angleToTarget);
-				} else {
-					turnLeftAngle = (2 * Math.PI) - currentOrientation
-							+ angleToTarget;
-					turnRightAngle = currentOrientation - angleToTarget;
-				}
+            if ((speed <= 720) && (Math.abs(angleToTarget) <= 5)) {
+                speed = speed + 100;
+                controller.forward(speed);
+                System.out.println("Speeding Up");
+            } else {
 
-				if (turnLeftAngle <= turnRightAngle)
-					controller.setWheelSpeeds(speed - 120, speed);
-					System.out.println("Turning Left");
-					//controller.setWheelSpeeds(speed, speed - 120);
-					//System.out.println("Blocking Right");
-					if (reachedBall == true) {
-						timer_travel = (System.currentTimeMillis() - timer_start) / 1000;
-						distance = distance + (float) (timer_travel * (speed - 120)/360);
-					}
-				if (turnLeftAngle > turnRightAngle)
-					controller.setWheelSpeeds(speed, speed - 120);
-					System.out.println("Turning Right");
-					//controller.setWheelSpeeds(speed - 120, speed);
-					//System.out.println("Blocking Left");
-					if (reachedBall == true) {
-						timer_travel = (System.currentTimeMillis() - timer_start) / 1000;
-						distance = distance + (float) (timer_travel * (speed - 120)/360);
-					}
+                double turnLeftAngle, turnRightAngle;
 
-			}
-			
-			if (distance > 30) {
-				controller.stop();
-			}
-			
-		}
+                if (angleToTarget > currentOrientation) {
+                    turnLeftAngle = angleToTarget - currentOrientation;
+                    turnRightAngle = currentOrientation
+                            + (2 * Math.PI - angleToTarget);
+                } else {
+                    turnLeftAngle = (2 * Math.PI) - currentOrientation
+                            + angleToTarget;
+                    turnRightAngle = currentOrientation - angleToTarget;
+                }
 
-	}
+                if (turnLeftAngle <= turnRightAngle) {
+                    // controller.setWheelSpeeds(speed - 120, speed);
+                    // System.out.println("Turning Left");
+                    controller.setWheelSpeeds(speed, speed - 120);
+                    System.out.println("Blocking Right");
+                }
+                if (turnLeftAngle > turnRightAngle) {
+                    // controller.setWheelSpeeds(speed, speed - 120);
+                    // System.out.println("Turning Right");
+                    controller.setWheelSpeeds(speed - 120, speed);
+                    System.out.println("Blocking Left");
+                }
+
+            }
+        }
+    }
 }
