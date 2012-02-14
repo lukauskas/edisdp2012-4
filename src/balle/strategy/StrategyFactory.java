8fdcfc167090d9d27d8d218452c43157514dbd40
Saulius Lukauskas <luksaulius@gmail.com>
2012-02-14 18:59:21 +0000

    Hacked aroudn a go to ball no kick strategy

--- src/balle/strategy/StrategyFactory.java
+++ src/balle/strategy/StrategyFactory.java
@@ -6,7 +6,8 @@ import balle.world.AbstractWorld;
 public class StrategyFactory {
     public static String[] availableDesignators() {
 
-        String[] designators = { "DummyStrategy", "GoToBall", "Dribble" };
+        String[] designators = { "DummyStrategy", "GoToBall", "GoToBallNoKick",
+                "Dribble" };
         return designators;
     }
 
@@ -18,6 +19,8 @@ public class StrategyFactory {
             return new DummyStrategy(controller, world);
         } else if (designator.equals("GoToBall")) {
             return new GoToBall(controller, world);
+        } else if (designator.equals("GoToBallNoKick")) {
+            return new GoToBallNoKick(controller, world);
         } else if (designator.equals("Dribble")) {
             return new Dribble(controller, world);
         } else
