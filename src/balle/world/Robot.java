ca7f5aee7660772b8d178cb33de76c0681cb6c96
Saulius Lukauskas <luksaulius@gmail.com>
2012-02-20 15:42:18 +0000

    Added stubs of functions to be implemented

--- src/balle/world/Robot.java
+++ src/balle/world/Robot.java
@@ -7,4 +7,32 @@ public class Robot extends RectangularObject {
     public Robot(Coord position, Velocity velocity, Orientation orientation) {
         super(position, velocity, orientation, Globals.ROBOT_WIDTH, Globals.ROBOT_LENGTH);
     }
+
+    /**
+     * Returns true if the robot is in possession of the ball. That is if the
+     * ball is close enough to the kicker that it can kick it.
+     * 
+     * @param ball
+     * @return true, if robot is in possession of the ball
+     */
+    public boolean possessesBall(Ball ball) {
+        // TODO: implement this.
+        return false;
+    }
+
+    /**
+     * Checks if the robot can score from this position. That is if it is in
+     * possession of the ball and facing the goal and other robot is is not
+     * blocking the path to the goal.
+     * 
+     * @param ball
+     * @param goal
+     * @param otherRobot
+     * @return true, if is in scoring position
+     */
+    public boolean isInScoringPosition(Ball ball, Goal goal, Robot otherRobot) {
+        // TODO: implement this
+        return false;
+    }
+
 }
