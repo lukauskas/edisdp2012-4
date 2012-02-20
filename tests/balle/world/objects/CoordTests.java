bea59908aca59259fc157fd87547e3e014c650ba
s0952880 <s0952880@kintra.inf.ed.ac.uk>
2012-02-20 20:11:41 +0000

    Fixed TestRunner bug.

---
+++ tests/balle/world/objects/CoordTests.java
@@ -0,0 +1,21 @@
+package balle.world.objects;
+
+import org.junit.Test;
+
+public class CoordTests {
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testIsReachableInStraightLineAndNotBlocked() {
+		
+	}
+	
+	public void testIsReachableInStraightLineAndNotBlocked2() {
+		
+		//Coord 
+		//FieldObject robot = new Robot()
+	}
+
+}
