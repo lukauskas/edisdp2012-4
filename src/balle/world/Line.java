5fafeee4e2daf3dac3e093ac23311ec30841a463
s0952880 <s0952880@tequila.inf.ed.ac.uk>
2012-02-21 20:25:05 +0000

    implemented line

--- src/balle/world/Line.java
+++ src/balle/world/Line.java
@@ -1,5 +1,20 @@
 package balle.world;
 
 public class Line {
+	
+	Coord a, b;
+	
+	public Coord getA() {
+		return a;
+	}
+	
+	public Coord getB() {
+		return b;
+	}
+	
+	public Line(Coord a, Coord b) {
+		this.a = a;
+		this.b = b;
+	}
 
 }
