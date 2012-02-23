8dff91fd188c59a3d0803b5e17876a5ba41dbadb
Neil MacLeod <s0965856@jaguar.inf.ed.ac.uk>
2012-02-23 15:08:08 +0000

    some work on BallNearWall strategy

--- src/balle/strategy/planner/KickFromWall.java
+++ src/balle/strategy/planner/KickFromWall.java
@@ -4,36 +4,53 @@ import balle.controller.Controller;
 import balle.strategy.executor.movement.MovementExecutor;
 import balle.world.AbstractWorld;
 import balle.world.Coord;
-import balle.world.objects.Ball;
+import balle.world.Snapshot;
+import balle.world.objects.FieldObject;
+import balle.world.objects.Location;
 
 public class KickFromWall extends AbstractPlanner {
-	
-	MovementExecutor            movementStrategy;
 
-	public KickFromWall(Controller controller, AbstractWorld world) {
+	MovementExecutor movementStrategy;
+
+	public KickFromWall(Controller controller, AbstractWorld world,
+			MovementExecutor movementStrategy) {
 		super(controller, world);
+		this.movementStrategy = movementStrategy;
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected void aiStep() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void aiMove(Controller controller) {
 		// TODO Auto-generated method stub
-		
+
 		if (getSnapshot() == null)
-            return;
-		
-		movementStrategy.updateState(getSnapshot());
-		
-        //movementStrategy.updateTarget();
-		
-		
-		
+			return;
+		Snapshot snap = getSnapshot();
+
+		double targetX = snap.getBall().getPosition().getX() - 0.04;
+		double targetY = snap.getBall().getPosition().getY() - 0.04;
+		Coord coord = new Coord(targetX, targetY);
+
+		movementStrategy.updateState(snap);
+
+		Location loc = new Location(coord);
+
+		movementStrategy.updateTarget((FieldObject) loc);
+
+		if (!movementStrategy.isFinished())
+			movementStrategy.step(controller);
+		else {
+			movementStrategy.stop(controller);
+			movementStrategy.updateTarget(snap.getBall());
+			controller.kick();
+		}
+
 	}
 
 }
---
+++ src/balle/world/objects/Location.java
@@ -0,0 +1,33 @@
+package balle.world.objects;
+
+import balle.world.Coord;
+import balle.world.Line;
+
+public class Location implements StaticFieldObject{
+
+	private Coord coord;
+	
+	public Location (Coord coord) {
+		this.coord = coord;
+	}
+	
+	@Override
+	public Coord getPosition() {
+		return coord;
+	}
+
+	@Override
+	public boolean containsCoord(Coord point) {
+		if (coord.getX() == point.getX() && coord.getY() == point.getY())
+			return true;
+		else
+			return false;
+	}
+
+	@Override
+	public boolean intersects(Line line) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
