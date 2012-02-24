0208393378dfa8b9d4d48fccf0383d7d05205ff1
Saulius Lukauskas <luksaulius@gmail.com>
2012-02-24 20:26:08 +0000

    added the midpoint of the line method

--- src/balle/world/Line.java
+++ src/balle/world/Line.java
@@ -1,39 +1,48 @@
 package balle.world;
 
-
 public class Line {
-	
-	private final Coord a, b;
-	
-	public Coord getA() {
-		return a;
-	}
-	
-	public Coord getB() {
-		return b;
-	}
-	
-	public Line(double x1, double y1, double x2, double y2) {
-		this.a = new Coord(x1, y1);
-		this.b = new Coord(x2, y2);
-	}
-	
-	public Line(Coord a, Coord b) {
-		this.a = a;
-		this.b = b;
-	}
 
-	/**
-	 * rotate the line around the origin
-	 * @param orientation
-	 * @return
-	 */
-	public Line rotate(Orientation orientation) {
-		return new Line(a.rotate(orientation), b.rotate(orientation));
-	}
+    private final Coord a, b;
 
-	public Line add(Coord position) {
-		return new Line(a.add(position), b.add(position));
-	}
+    public Coord getA() {
+        return a;
+    }
 
+    public Coord getB() {
+        return b;
+    }
+
+    public Line(double x1, double y1, double x2, double y2) {
+        this.a = new Coord(x1, y1);
+        this.b = new Coord(x2, y2);
+    }
+
+    public Line(Coord a, Coord b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    /**
+     * rotate the line around the origin
+     * 
+     * @param orientation
+     * @return
+     */
+    public Line rotate(Orientation orientation) {
+        return new Line(a.rotate(orientation), b.rotate(orientation));
+    }
+
+    public Line add(Coord position) {
+        return new Line(a.add(position), b.add(position));
+    }
+
+    /**
+     * Returns the midpoint of the line
+     * 
+     * @return
+     */
+    public Coord midpoint() {
+        return new Coord((getA().getX() + getB().getY()) / 2.0,
+                (getA().getY() + getB().getY()) / 2.0);
+    }
 }
