6ffaf5891f9c0b63331c38320ee974032ab4e572
Saulius Lukauskas <luksaulius@gmail.com>
2012-02-24 15:23:45 +0000

    removed kickcapaitytest from strategyfactory

--- src/balle/strategy/StrategyFactory.java
+++ src/balle/strategy/StrategyFactory.java
@@ -7,14 +7,13 @@ import balle.strategy.executor.turning.FaceAngle;
 import balle.strategy.planner.AbstractPlanner;
 import balle.strategy.planner.DribbleMilestone2;
 import balle.strategy.planner.GoToBall;
-import balle.strategy.planner.KickCapacityTest;
 import balle.world.AbstractWorld;
 
 public class StrategyFactory {
     public static String[] availableDesignators() {
 
         String[] designators = { "GoToBall", "GoToBallPFN", "Dribble",
-                "Blocking", "PFNavigation", "KickCapacityTest" };
+                "Blocking", "PFNavigation" };
         return designators;
     }
 
@@ -35,8 +34,6 @@ public class StrategyFactory {
             return new DribbleMilestone2(controller, world);
         } else if (designator.equals("PFNavigation")) {
             return new PFNavigation(controller, world);
-        } else if (designator.equals("KickCapacityTest")) {
-            return new KickCapacityTest(controller, world);
         } else
             throw new UnknownDesignatorException("Don't know strategy \""
                     + designator + "\"");
