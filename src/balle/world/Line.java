9106ea591b13fa8d8636fbdbac7b06e3485848d0
David Eichmann <EichmannD@gmail.com>
2012-02-24 21:33:04 +0000

    lol

--- src/balle/world/Line.java
+++ src/balle/world/Line.java
@@ -1,5 +1,8 @@
 package balle.world;
 
+import java.awt.geom.Line2D;
+import java.awt.geom.Line2D.Double;
+
 public class Line {
 
     private final Coord a, b;
@@ -36,10 +39,51 @@ public class Line {
         return new Line(a.add(position), b.add(position));
     }
     
-    public Coord getIntersect() {
-    	
-    }
+    public Coord getIntersect(Line l) {
+//		double m1 = (b.getY()-y1)/(b.getX()-x1);
+//		double m2 = (l.getB().getY()-y2)/(l.getB().getX()-x2);
 
+    	double x1,x2,y1,y2;
+    	
+    	x1 = a.getX();
+		y1 = a.getY();
+		x2 = b.getX();
+		y2 = b.getY();
+		double a1 = y2-y1;
+		double b1 = x1-x2;
+		double c1 = (a1*x1)+(b1*y1);
+
+		x1 = l.getA().getX();
+		y1 = l.getA().getY();
+		x2 = l.getB().getX();
+		y2 = l.getB().getY();
+		double a2 = y2-y1;
+		double b2 = x1-x2;
+		double c2 = (a2*x1)+(b2*y1);
+		
+		double det = a1*b2 - a2*b1;
+	    if(det == 0){
+	    	// lines are parallel
+	        return null;
+	    }
+	    else{
+	        double x = (b2*c1 - b1*c2)/det;
+	        double y = (a1*c2 - a2*c1)/det;
+	        
+	        contains();
+	    }
+	    
+	    
+    }
+    
+    public boolean overLaps(Line l) {
+    	return intersects(l) && 
+    }
+    
+    public boolean intersects(Line l) {
+    	return getLine2DVersion().intersectsLine(l.getLine2DVersion());
+    }
+    
     /**
      * Returns the midpoint of the line
      * 
@@ -48,5 +92,9 @@ public class Line {
     public Coord midpoint() {
         return new Coord((getA().getX() + getB().getY()) / 2.0,
                 (getA().getY() + getB().getY()) / 2.0);
+    }
+    
+    private Line2D getLine2DVersion() {
+    	return new Line2D.Double(a.getX(), a.getY(), b.getX(), b.getY());
     }
 }
