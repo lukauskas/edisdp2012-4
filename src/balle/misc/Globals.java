a783ef1eea9b1b68344c065142d095c0ad2bca49
David Eichmann <EichmannD@gmail.com>
2012-02-24 18:28:46 +0000

    Cap the motor speed to MAXIMUM_MOTOR_SPEED

--- src/balle/misc/Globals.java
+++ src/balle/misc/Globals.java
@@ -34,6 +34,8 @@ public class Globals {
     public static final int    MAXIMUM_MOTOR_SPEED       = 900;
 
 	public static float powerToVelocity(float p) {
+		if(p > MAXIMUM_MOTOR_SPEED) p = MAXIMUM_MOTOR_SPEED;
+		if(p < -MAXIMUM_MOTOR_SPEED) p = -MAXIMUM_MOTOR_SPEED;
 		return p * (0.4f/720f);
 	//	if(p==0) return 0;
 	//	boolean isNeg = p < 0;
