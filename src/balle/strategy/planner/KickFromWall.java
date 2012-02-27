0428cfa26e9fabedca2572fc804039096ac7ae3d
Mindaugas Dabulskis <mindausai@gmail.com>
2012-02-27 21:47:21 +0000

    fixing BallNearWall startegy

--- src/balle/strategy/planner/KickFromWall.java
+++ src/balle/strategy/planner/KickFromWall.java
@@ -4,8 +4,9 @@ import org.apache.log4j.Logger;
 
 import balle.controller.Controller;
 import balle.strategy.executor.movement.GoToObject;
+import balle.strategy.executor.movement.GoToObjectPFN;
 import balle.strategy.executor.movement.MovementExecutor;
-import balle.strategy.executor.turning.FaceAngle;
+import balle.strategy.executor.turning.IncFaceAngle;
 import balle.world.Coord;
 import balle.world.Snapshot;
 import balle.world.objects.Point;
@@ -18,6 +19,7 @@ public class KickFromWall extends GoToBall {
 	boolean secondStep = false;
 	boolean additionalStep = false;
 	boolean kicked = false;
+	boolean goingToBall = false;
 
 	public KickFromWall(MovementExecutor movementStrategy) {
 		super(movementStrategy);
@@ -79,7 +81,7 @@ public class KickFromWall extends GoToBall {
 		}
 
 		if (!secondStep) {
-			// LOG.info("Going to location");
+			LOG.info("Going to location");
 
 			if (Math.abs(snap.getBalle().getPosition().getY()
 					- snap.getBall().getPosition().getY()) < 0.3
@@ -90,9 +92,20 @@ public class KickFromWall extends GoToBall {
 				return loc;
 			}
 		} else {
-			// LOG.info("Going to ball");
-			MovementExecutor strategy = new GoToObject(new FaceAngle());
-			strategy.setStopDistance(0);
+
+			goingToBall = true;
+
+			MovementExecutor strategy;
+
+			if (snap.getBalle().getPosition()
+					.dist(snap.getBall().getPosition()) < 0.5) {
+				LOG.info("Going to ball normally");
+				strategy = new GoToObject(new IncFaceAngle());
+				strategy.setStopDistance(0);
+			} else {
+				LOG.info("Going to ball with PFN");
+				strategy = new GoToObjectPFN(0);
+			}
 			setExecutorStrategy(strategy);
 			return loc2;
 		}
@@ -106,7 +119,8 @@ public class KickFromWall extends GoToBall {
 
 	@Override
 	public void step(Controller controller) {
-		if (getSnapshot().getBall().isNear(getSnapshot().getBalle()))
+		if (getSnapshot().getBall().isNear(getSnapshot().getBalle())
+				&& goingToBall)
 			controller.kick();
 		super.step(controller);
 	}
