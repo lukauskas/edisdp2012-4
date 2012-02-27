1317ba2be40e466e6dd5966c824e7a552e8fc1ca
s0952880 <s0952880@lada.inf.ed.ac.uk>
2012-02-27 23:18:03 +0000

    Speeded up IncFaceAngle

--- src/balle/strategy/Game.java
+++ src/balle/strategy/Game.java
@@ -6,6 +6,7 @@ import balle.controller.Controller;
 import balle.strategy.executor.turning.IncFaceAngle;
 import balle.strategy.executor.turning.RotateToOrientationExecutor;
 import balle.strategy.planner.AbstractPlanner;
+import balle.world.Coord;
 import balle.world.Orientation;
 import balle.world.Snapshot;
 import balle.world.objects.Ball;
@@ -89,9 +90,34 @@ public class Game extends AbstractPlanner {
 					// use the controller.rotate()
 					// command that is blocking.
 
-					System.out.println(ourRobot
-							.findMaxRotationMaintaintingPossession(ball, true));
-					LOG.info("robot facing wrong way, shouldnt shoot");
+					Coord r, b, g;
+					r = ourRobot.getPosition();
+					b = ball.getPosition();
+					g = ownGoal.getPosition();
+
+					if (r.angleBetween(g, b).atan2styleradians() < 0) {
+						// Clockwise.
+						Orientation orien = ourRobot
+								.findMaxRotationMaintaintingPossession(ball,
+										true);
+						System.out.println(orien);
+						turningExecutor.setTargetOrientation(orien);
+						turningExecutor.step(controller);
+						if (ourRobot.findMaxRotationMaintaintingPossession(
+								ball, true).degrees() < 10)
+							controller.kick();
+					} else {
+						// Anti-Clockwise
+						Orientation orien = ourRobot
+								.findMaxRotationMaintaintingPossession(ball,
+										false);
+						System.out.println(orien);
+						turningExecutor.setTargetOrientation(orien);
+						turningExecutor.step(controller);
+						if (ourRobot.findMaxRotationMaintaintingPossession(
+								ball, false).degrees() > -10)
+							controller.kick();
+					}
 				}
 			}
 		} else if ((opponent.possessesBall(ball))
