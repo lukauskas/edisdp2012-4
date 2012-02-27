5b0a7f9ed1a1b74b845bee24dbe908b80b3d3a14
s0951368 <s0951368@penner.inf.ed.ac.uk>
2012-02-27 21:08:43 +0000

    trying penalty blocking

--- src/balle/strategy/GameFromPenaltyDefence.java
+++ src/balle/strategy/GameFromPenaltyDefence.java
@@ -6,41 +6,59 @@ import balle.world.objects.Goal;
 import balle.world.objects.Robot;
 
 public class GameFromPenaltyDefence extends Game {
+	double lastknowngoalposstionx;
 
-    public GameFromPenaltyDefence() throws UnknownDesignatorException {
-        super();
-    }
+	public GameFromPenaltyDefence() throws UnknownDesignatorException {
+		super();
+	}
 
-    /**
-     * Checks if robot is is still defending the penalty;
-     * 
-     * @return
-     */
-    public boolean isStillInPenaltyDefence() {
-        // TODO: implement
-        // Probably worth checking the distance of the ball to the goal line
-        // here
-        return true;
-    }
+	/**
+	 * Checks if robot is is still defending the penalty;
+	 * 
+	 * @return
+	 */
+	public boolean isStillInPenaltyDefence() {
+		Ball ball = getSnapshot().getBall();
+		Goal ownGoal = getSnapshot().getOwnGoal();
+		if (ball.intersects(ownGoal.getGoalLine())
+				|| (ball.getPosition().getX() > lastknowngoalposstionx && ownGoal
+						.isLeftGoal())
+				|| (ball.getPosition().getX() < lastknowngoalposstionx && ownGoal
+						.isRightGoal())) {
+			return false;
+		}
+		return true;
+	}
 
-    @Override
-    public void step(Controller controller) {
+	@Override
+	public void step(Controller controller) {
 
-        if (isStillInPenaltyDefence()) {
-            Robot ourRobot = getSnapshot().getBalle();
-            Robot opponent = getSnapshot().getOpponent();
-            Ball ball = getSnapshot().getBall();
-            Goal ownGoal = getSnapshot().getOwnGoal();
+		if (isStillInPenaltyDefence()) {
+			Robot ourRobot = getSnapshot().getBalle();
+			Robot opponent = getSnapshot().getOpponent();
+			Ball ball = getSnapshot().getBall();
+			Goal ownGoal = getSnapshot().getOwnGoal();
 
-            // TODO: Do Penalty defence here
-            // Try using ourRobot.getFacingLineBothWays() and
-            // opponent.getBallKickLine(ball)
-            // And finding their intersection and then moving to that point.
-            // (Youll have to manually
-            // set wheel speeds to positive or negative values here.
-        } else {
-            super.step(controller);
-        }
-    }
+			lastknowngoalposstionx = ball.getPosition().getX();
 
+			double bcrosspointy = ourRobot.getFacingLineBothWays()
+					.getIntersect(opponent.getBallKickLine(ball)).getY();
+
+			double ourypossition = ourRobot.getPosition().getY();
+
+			double travely = (bcrosspointy - ourypossition);
+			if (bcrosspointy == ourRobot.getPosition().getY()) {
+				controller.stop();
+			} else {
+				if (travely > 0) {
+					controller.forward(580);
+				} else {
+					controller.backward(580);
+				}
+			}
+		} else {
+			controller.stop();
+			super.step(controller);
+		}
+	}
 }
