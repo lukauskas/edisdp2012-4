7763339df55ac4bf54790b879c46f8363887e825
Paulius <s0943242@trool.inf.ed.ac.uk>
2012-02-28 21:40:16 +0000

    PenaltyDefence Strategy implemented v5.12

--- src/balle/strategy/GameFromPenaltyDefence.java
+++ src/balle/strategy/GameFromPenaltyDefence.java
@@ -1,82 +1,108 @@
 package balle.strategy;
 
+import org.apache.log4j.Logger;
+
 import balle.controller.Controller;
-import balle.world.objects.Ball;
-import balle.world.objects.Goal;
-import balle.world.objects.Robot;
+import balle.world.Orientation;
+import balle.world.Snapshot;
 
 public class GameFromPenaltyDefence extends Game {
-	double lastknownxballpos;
+
+	private static Logger LOG = Logger.getLogger(GameFromPenaltyDefence.class);
+
+	private Snapshot firstSnapshot = null;
+	String robotState = "Center";
+	int rotateSpeed = 0;
 
 	public GameFromPenaltyDefence() throws UnknownDesignatorException {
 		super();
 	}
 
-	/**
-	 * Checks if robot is is still defending the penalty;
-	 * 
-	 * @return
-	 */
 	public boolean isStillInPenaltyDefence() {
-		Ball ball = getSnapshot().getBall();
-		Goal ownGoal = getSnapshot().getOwnGoal();
-		// Robot opponent = getSnapshot().getOpponent();
-		// fix if boolean expression for when penalty saved or scored
-		if (!ball.intersects(ownGoal.getGoalLine())
-				&& !(ball.getVelocity().getX() > 0 || ball.getVelocity().getY() > 0)) {
-			return true;
-		} else {
 
+		if (firstSnapshot == null)
+			return true;
+
+		double ball_x = firstSnapshot.getBall().getPosition().getX();
+		double ball_y = firstSnapshot.getBall().getPosition().getY();
+
+		double aball_x = getSnapshot().getBall().getPosition().getX();
+		double aball_y = getSnapshot().getBall().getPosition().getY();
+
+		double distance = Math.sqrt(Math.pow(aball_x - ball_x, 2)
+				+ Math.pow(aball_y - ball_y, 2));
+
+		if (distance > 1) {
 			return false;
 		}
+
+		return true;
 	}
 
 	@Override
 	public void step(Controller controller) {
 
-		if (isStillInPenaltyDefence()) {
-			Robot ourRobot = getSnapshot().getBalle();
-			Robot opponent = getSnapshot().getOpponent();
-			Ball ball = getSnapshot().getBall();
-			Goal ownGoal = getSnapshot().getOwnGoal();
+		if ((firstSnapshot == null)
+				&& (getSnapshot().getBall().getPosition() != null))
+			firstSnapshot = getSnapshot();
 
-			double ourypossition = ourRobot.getPosition().getY();
+		Orientation opponentAngle = getSnapshot().getOpponent()
+				.getOrientation();
+		double threshold = Math.toRadians(20);
+		Boolean isLeftGoal = getSnapshot().getOwnGoal().isLeftGoal();
 
-			double bcrosspointy = ourypossition;
-			if (ourRobot.getFacingLineBothWays().intersects(
-					opponent.getBallKickLine(ball))) {
-				bcrosspointy = ourRobot.getFacingLineBothWays()
-						.getIntersect(opponent.getBallKickLine(ball)).getY();
-			}
-			double y = ourRobot.getFacingLineBothWays()
-					.getIntersect(opponent.getBallKickLine(ball)).getY();
+		if (getSnapshot().getOwnGoal().getMaxY() <= getSnapshot().getBalle()
+				.getPosition().getY() + 0.2) {
+			robotState = "Up";
+		} else if (getSnapshot().getOwnGoal().getMinY() >= getSnapshot()
+				.getBalle().getPosition().getY() - 0.2) {
+			robotState = "Down";
+		} else {
+			robotState = "Center";
+		}
 
-			double travely = (bcrosspointy * 10 - ourypossition * 10);
+		LOG.debug("robotState: " + robotState);
 
-			if (bcrosspointy >= ourRobot.getPosition().getY()
-					&& bcrosspointy <= ourRobot.getPosition().getY() + 0.02) {
-				controller.stop();
-			} else {
-				if (y > ownGoal.getMinY() && y < ownGoal.getMaxY()) {
-					// add if robot betwee y of goal
-					if (travely > 0) {
-
-						controller.forward(580);
-
-					} else {
-
-						controller.backward(580);
-
-					}
-				} else {
-					controller.stop();
-				}
-
-			}
+		if (isLeftGoal == true) {
+			// opponent shooting left
+			if (opponentAngle.atan2styleradians() > Math.PI - threshold)
+				moveTo("Up", controller);
+			else if (opponentAngle.atan2styleradians() < -Math.PI + threshold)
+				moveTo("Down", controller);
+			else
+				moveTo("Center", controller);
 
 		} else {
-			System.out.println("OUT");
-			super.step(controller);
+			// opponent shooting right
+			if (opponentAngle.atan2styleradians() > threshold)
+				moveTo("Up", controller);
+			else if (opponentAngle.atan2styleradians() < -threshold)
+				moveTo("Down", controller);
+			else
+				moveTo("Center", controller);
 		}
+
 	}
+
+	private void moveTo(String moveTo, Controller controller) {
+
+		rotateSpeed = 0;
+		LOG.debug("moveTo: " + moveTo);
+
+		if (robotState.equals("Center") && moveTo.equals("Up"))
+			rotateSpeed = 500;
+		if (robotState.equals("Center") && moveTo.equals("Down"))
+			rotateSpeed = -700;
+		if (robotState.equals("Up") && moveTo.equals("Center"))
+			rotateSpeed = -700;
+		if (robotState.equals("Up") && moveTo.equals("Down"))
+			rotateSpeed = -500;
+		if (robotState.equals("Down") && moveTo.equals("Center"))
+			rotateSpeed = 500;
+		if (robotState.equals("Down") && moveTo.equals("Up"))
+			rotateSpeed = 700;
+
+		controller.setWheelSpeeds(rotateSpeed, rotateSpeed);
+	}
+
 }
--- src/balle/strategy/GameFromPenaltyKick.java
+++ src/balle/strategy/GameFromPenaltyKick.java
@@ -6,26 +6,30 @@ import balle.controller.Controller;
 
 public class GameFromPenaltyKick extends Game {
 
-    private static final Logger LOG        = Logger.getLogger(GameFromPenaltyKick.class);
+	private static final Logger LOG = Logger
+			.getLogger(GameFromPenaltyKick.class);
 
-    public long                 timeToKick = 0;
-    public long                 timeToGame = 0;
+	public long timeToKick = 0;
+	public long timeToGame = 0;
 
-    public GameFromPenaltyKick() throws UnknownDesignatorException {
-        super();
-    }
+	public GameFromPenaltyKick() throws UnknownDesignatorException {
+		super();
+	}
 
-    @Override
-    public void step(Controller controller) {
-        if (timeToKick == 0)
-            timeToKick = System.currentTimeMillis() + 1000;
-        else if ((timeToKick <= System.currentTimeMillis()) && (timeToGame == 0)) {
-            LOG.info("Kicking penalty!");
-            timeToGame = System.currentTimeMillis() + 2000;
-            controller.penaltyKick();
-        } else if ((timeToGame != 0) && (timeToGame <= System.currentTimeMillis())) {
-            super.step(controller);
-        }
-    }
+	@Override
+	public void step(Controller controller) {
+		if (timeToKick == 0) {
+			timeToKick = System.currentTimeMillis() + 1000;
+		} else if ((timeToKick <= System.currentTimeMillis())
+				&& (timeToGame == 0)) {
+			LOG.info("Kicking penalty!");
+			timeToGame = System.currentTimeMillis() + 2000;
+			controller.penaltyKick();
+		} else if ((timeToGame != 0)
+				&& (timeToGame <= System.currentTimeMillis())) {
+			super.step(controller);
+		}
+
+	}
 
 }
