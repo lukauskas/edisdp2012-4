0878e2549e4d9072932449c7f17796988cf58cb4
s0952880 <s0952880@dunoon.inf.ed.ac.uk>
2012-02-28 20:27:02 +0000

    Added correct camera heights

--- src/balle/world/processing/HeightFilter.java
+++ src/balle/world/processing/HeightFilter.java
@@ -3,39 +3,43 @@ package balle.world.processing;
 import balle.world.Coord;
 
 /**
- * Class for filtering input from world to account for height
- * differences between the detected position and the real position
- * at ground level.
+ * Class for filtering input from world to account for height differences
+ * between the detected position and the real position at ground level.
  * 
  * @author s0952880
  */
 public class HeightFilter {
-	
-	/** Point directly below camera.
+
+	/**
+	 * Point directly below camera.
 	 */
 	private Coord worldCenter;
-	
-	/** Height of camera above worldCenter.
+
+	/**
+	 * Height of camera above worldCenter.
 	 */
 	private double cameraHeight;
-	
-	/** Constructor for HeightFilter.
+
+	/**
+	 * Constructor for HeightFilter.
 	 * 
-	 * @param worldCenter Point directly below the camera.
-	 * @param cameraHeight The height of the camera above worldCenter.
+	 * @param worldCenter
+	 *            Point directly below the camera.
+	 * @param cameraHeight
+	 *            The height of the camera above worldCenter.
 	 */
 	public HeightFilter(Coord worldCenter, double cameraHeight) {
 		this.worldCenter = worldCenter;
 		this.cameraHeight = cameraHeight;
 	}
-	
-	private Coord filter(Coord in, double height) {
+
+	public Coord filter(Coord in, double height) {
 		Coord d = in.sub(worldCenter);
-		
-		double ratio = height/cameraHeight;
+
+		double ratio = height / cameraHeight;
 		d.mult(ratio);
 		d.add(worldCenter);
-		
+
 		return d;
 	}
 
