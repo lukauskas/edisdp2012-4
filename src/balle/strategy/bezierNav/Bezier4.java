f757d5d349926d7f477cff79910c0fd74f0b67e2
james <james@James-Laptop>
2012-03-01 17:06:08 +0000

    Added Bezier4 class

---
+++ src/balle/strategy/bezierNav/Bezier4.java
@@ -0,0 +1,30 @@
+package balle.strategy.bezierNav;
+
+import static java.lang.Math.pow;
+import balle.world.Coord;
+
+public class Bezier4 {
+
+	private Coord[] p;
+
+	public Bezier4(Coord[] coords) {
+		this.p = coords;
+	}
+
+	public Coord pos(double t) {
+		return p[0].mult(pow(1 - t, 3)).add(p[1].mult(3 * pow(1 - t, 2) * t))
+				.add(p[2].mult(3 * (1 - t) * t * t)).add(p[3].mult(pow(t, 3)));
+	}
+
+	public Coord vel(double t) {
+		return p[0].mult(-3 + (6 * t) - (3 * t * t))
+				.add(p[1].mult(3 * (1 - (4 * t) + (3 * t * t))))
+				.add(p[2].mult(3 * ((2 * t) - (3 * t * t))))
+				.add(p[3].mult(3 * t * t));
+	}
+	
+	public Coord acc(double t) {
+		return p[0].mult(6 - (6 * t)).add(p[1].mult(3 * (-4 + (6 * t))))
+				.add(p[2].mult(3 * (2 - (6 * t)))).add(p[3].mult(6 * t));
+	}
+}
