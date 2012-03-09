e80ad22b54dd1a5b5e9238342379a2bdcd6ef44c
Saulius Lukauskas <luksaulius@gmail.com>
2012-03-09 18:28:54 +0000

    some sample behaviour for KickToGoal

--- src/balle/strategy/planner/KickToGoal.java
+++ src/balle/strategy/planner/KickToGoal.java
@@ -3,6 +3,7 @@ package balle.strategy.planner;
 import org.apache.log4j.Logger;
 
 import balle.controller.Controller;
+import balle.misc.Globals;
 import balle.world.Snapshot;
 import balle.world.objects.Ball;
 import balle.world.objects.Goal;
@@ -42,49 +43,34 @@ public class KickToGoal extends AbstractPlanner {
                 controller.setWheelSpeeds(200, 200);
             } else if (ourRobot.isFacingGoal(goal)) {
                 LOG.info("We are facing goal, but opponent is blocking the shot");
-                // TODO: Turn slightly here (a few degrees) to trigger the
-                // bounce from wall
-                // kick in the else statement below
-                // Use IncFaceAngle executor here
-                // SOme code that could be useful:
-
-                // TODO: turn the robot slightly so we face away from our
-                // own goal.
-                // Implement a turning executor that would use
-                // setWheelSpeeds to some arbitrary low
-                // number (say -300,300 and 300,-300) to turn to correct
-                // direction and use it here.
-                // it has to be similar to FaceAngle executor but should not
-                // use the controller.rotate()
-                // command that is blocking.
-
-                // Coord r, b, g;
-                // r = ourRobot.getPosition();
-                // b = ball.getPosition();
-                // g = ownGoal.getPosition();
-                //
-                // if (r.angleBetween(g, b).atan2styleradians() < 0) {
-                // // Clockwise.
-                // Orientation orien = ourRobot
-                // .findMaxRotationMaintaintingPossession(ball, true);
-                // System.out.println(orien);
-                // turningExecutor.setTargetOrientation(orien);
-                // turningExecutor.step(controller, snapshot);
-                // if (ourRobot.findMaxRotationMaintaintingPossession(ball,
-                // true).degrees() < 10)
-                // controller.kick();
-                // } else {
-                // // Anti-Clockwise
-                // Orientation orien = ourRobot
-                // .findMaxRotationMaintaintingPossession(ball, false);
-                // System.out.println(orien);
-                // turningExecutor.setTargetOrientation(orien);
-                // turningExecutor.step(controller, snapshot);
-                // if (ourRobot.findMaxRotationMaintaintingPossession(ball,
-                // false).degrees() > -10)
-                // controller.kick();
-                // }
-                //
+                if (ourRobot.isFacingLeft())
+                {
+                    if (ourRobot.getPosition().getY() > Globals.PITCH_HEIGHT/2)
+                    {
+                        // If we're facing left and in upper half of the pitch, turn right
+                        controller.setWheelSpeeds(700, 600);
+                    }
+                    else
+                    {
+                        // Turn left
+                        controller.setWheelSpeeds(600, 700);
+                    }
+                }
+                else
+                {
+                    if (ourRobot.getPosition().getY() > Globals.PITCH_HEIGHT/2)
+                    {
+                        // If we're facing right and in upper half of the pitch, turn left
+                        controller.setWheelSpeeds(600, 700);
+                    }
+                    else
+                    {
+                        // Else
+                        // Turn right
+                        controller.setWheelSpeeds(700, 600);
+                    }
+                }
+                return;
             } else {
                 LOG.info("We're not facing goal");
                 // TODO: Check if it is better to kick the ball from this
