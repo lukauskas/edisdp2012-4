6db0ccbd206bb46ad77c52186fbb156f460e4adc
neil-macleod <neilsjmacleod@yahoo.co.uk>
2012-03-15 00:02:39 +0000

    new powerToVelocity method added to misc.Globals and class Powers for velocity and power fields

---
+++ src/balle/misc/Powers.java
@@ -0,0 +1,20 @@
+package balle.misc;
+
+public class Powers {
+	private final int power;
+	private final float velocity;
+
+	public int getPower() {
+		return power;
+	}
+
+	public float getVelocity() {
+		return velocity;
+	}
+
+	public Powers(int power, float velocity) {
+		this.power = power;
+		this.velocity = velocity;
+	}
+
+}
