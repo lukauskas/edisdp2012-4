6a7cdaf64cdec07d0cf916be12a4a0e942170d25
s0952880 <s0952880@ford.inf.ed.ac.uk>
2012-03-20 21:31:45 +0000

    Added data

---
+++ src/balle/misc/VtoPData.java
@@ -0,0 +1,27 @@
+package balle.misc;
+
+public class VtoPData {
+
+	public Powers[] getData() {
+		return new Powers[] { new Powers(0, 3.54E-007f),
+				new Powers(25, 1.75E-005f), new Powers(50, 4.05E-005f),
+				new Powers(75, 5.83E-005f), new Powers(100, 8.14E-005f),
+				new Powers(125, 9.79E-005f), new Powers(150, 1.19E-004f),
+				new Powers(175, 1.37E-004f), new Powers(200, 1.56E-004f),
+				new Powers(225, 1.71E-004f), new Powers(250, 1.90E-004f),
+				new Powers(275, 2.07E-004f), new Powers(300, 2.29E-004f),
+				new Powers(325, 2.43E-004f), new Powers(350, 2.66E-004f),
+				new Powers(375, 2.81E-004f), new Powers(400, 3.00E-004f),
+				new Powers(425, 3.23E-004f), new Powers(450, 3.40E-004f),
+				new Powers(475, 3.59E-004f), new Powers(500, 3.77E-004f),
+				new Powers(525, 3.90E-004f), new Powers(550, 4.13E-004f),
+				new Powers(575, 4.33E-004f), new Powers(600, 4.39E-004f),
+				new Powers(625, 4.45E-004f), new Powers(650, 4.54E-004f),
+				new Powers(675, 4.70E-004f), new Powers(700, 4.68E-004f),
+				new Powers(725, 4.79E-004f), new Powers(750, 4.82E-004f),
+				new Powers(775, 4.83E-004f), new Powers(800, 4.88E-004f),
+				new Powers(825, 4.81E-004f), new Powers(850, 4.77E-004f),
+				new Powers(875, 4.78E-004f), new Powers(900, 4.81E-004f), };
+	}
+
+}
