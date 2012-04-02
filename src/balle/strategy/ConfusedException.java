36883209b63972b049d03113bbcc5dc6d9191847
Saulius Lukauskas <luksaulius@gmail.com>
2012-04-02 22:51:09 +0100

    added constructors for confused exceptions

--- src/balle/strategy/ConfusedException.java
+++ src/balle/strategy/ConfusedException.java
@@ -2,4 +2,24 @@ package balle.strategy;
 
 public class ConfusedException extends Exception {
 
+    public ConfusedException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public ConfusedException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ConfusedException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ConfusedException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
 }
